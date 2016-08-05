cf0f54f242a6a3292836f003b35d58c2cd8825b4
elaiyabharathi <elaiyabharathi@hotmail.com>
2016-08-05 15:11:45 +0530

    Update TestMain.java

--- TestMain.java
+++ TestMain.java
@@ -1,8 +1,10 @@
+package previousTeams;
+
 public class TestMain {
     public static void main(String[] args) {
-        Rack r = new Rack ("awesome");
-        Word w = new Word ("awesome");
-        System.out.println (w.computePoints());
+        Rack r = new Rack ("AWQER T");
+        //Word w = new Word ("awesome");
+        //System.out.println (w.computePoints());
         System.out.println (r.getBestLegalWord());
     }
 }
